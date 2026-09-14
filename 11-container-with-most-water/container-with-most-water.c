c10f72c5240bc0ab1f8d272c15fd5c0a10504116
kruthi <kruthi.connect@gmail.com>
2026-09-14 21:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (84.42%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.c
@@ -0,0 +1,30 @@
+int maxArea(int* height, int heightSize)
+{
+    int left = 0;
+    int right = heightSize - 1;
+    int maxWater = 0;
+
+    while (left < right)
+    {
+        int width = right - left;
+
+        int h;
+        if (height[left] < height[right])
+            h = height[left];
+        else
+            h = height[right];
+
+        int area = width * h;
+
+        if (area > maxWater)
+            maxWater = area;
+
+        // Move the shorter line
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+
+    return maxWater;
+}
